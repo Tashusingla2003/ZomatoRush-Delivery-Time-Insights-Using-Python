a8c62b027cbaec3ee26b02996df1ee1ce615d426
Tashu Singla <tashusingla03@gmail.com>
2025-07-20 13:06:37 -0700

    Added sample data set file

---
+++ sample data set.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 1. Load dataset
+df = pd.read_csv("C:\\Users\\DELL\\Desktop\\sample_food_delivery_data (1).csv")
+print(df)
